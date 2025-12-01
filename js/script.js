fb8c9e4adede1ca6904f9ee23560f78cdcb5df54
Guilherme Almeida <guilhermedev24@gmail.com>
2025-12-01 15:16:43 +0000

    Remove API key from API_URL and SEARCH_API
    Removed API key from API_URL and SEARCH_API.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-const API_URL ='https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=7fa75a49a0388b7208e905f710994540&page=1';
+const API_URL ='https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?&api_key=7fa75a49a0388b7208e905f710994540&query="';
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?&api_key=';
 
 const form = document.getElementById('form');
 const search = document.getElementById('search');
